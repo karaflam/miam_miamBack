2c7997f5da4fc9d285616a10e3474c8263fb9c0a
karaflam <marc.fangue@2029.ucac-icam.com>
2025-10-23 19:54:38 +0100

    Update app.php

--- miam_miam/bootstrap/app.php
+++ miam_miam/bootstrap/app.php
@@ -16,6 +16,19 @@ return Application::configure(basePath: dirname(__DIR__))
             \Illuminate\Http\Middleware\AddLinkHeadersForPreloadedAssets::class,
         ]);
 
+        $middleware->api(prepend: [
+            \Laravel\Sanctum\Http\Middleware\EnsureFrontendRequestsAreStateful::class,
+        ]);
+
+        // Important : Ajouter ces middlewares
+        $middleware->encryptCookies(except: [
+            //
+        ]);
+
+        $middleware->validateCsrfTokens(except: [
+            '/api/*', // Exclure les routes API du CSRF
+        ]);
+
         //
     })
     ->withExceptions(function (Exceptions $exceptions): void {
